fa4efcb506e095bfbbef81fffd824de3a21b72f1
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-06-09 22:37:41 +0200

    added chezmoi

--- basic-code.sh
+++ basic-code.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-brew install --cask iterm2
-brew install --cask coconutbattery
-brew install --cask amethyst
 brew install --cask visual-studio-code
 brew install --cask github
--- basic.sh
+++ basic.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+brew install chezmoi
+brew install --cask iterm2
+brew install --cask coconutbattery
+brew install --cask amethyst
 brew install --cask discord
 brew install --cask telegram-desktop
