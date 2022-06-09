933179136d4beccc2bb121d69b7c50d507a4ceb5
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-06-09 19:22:39 +0200

    corrected zsh-autosuggestion theme

--- basic-zsh.sh
+++ basic-zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE=\'fg=60\' >> $ZSH_CUSTOM/zsh-autosuggestions_custom.zsh
