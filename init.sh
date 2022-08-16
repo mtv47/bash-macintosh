627e66ced8dd8fb889e9959a6ced9b79d3b3e85b
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-16 22:23:45 +0200

    added xcode-select

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ sudo scutil --set ComputerName "macintosh"
 sudo scutil --set LocalHostName "macintosh"
 sudo scutil --set HostName "macintosh"
 
+#Install xcode-select
+xcode-select —install
+
 #Basic.sh
 sudo bash defaults.sh
 sudo bash brew.sh
