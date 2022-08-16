5f47213907d5f683c0de53b977358b2288740a01
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-16 18:30:24 +0200

    modified and added brew installs

---
+++ brew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Basic cask
+brew install --cask brave-browser
+brew install --cask kitty
+brew install --cask coconutbattery
+brew install --cask turbo-boost-switcher
+brew install --cask amethyst
+brew install htop
+
+#Basic installs
+brew install htop
+brew install wget
+brew install mas
+brew install speedtest-cli
+
+#Torrenting
+brew install --cask protonvpn
+brew install --cask deluge
+
+#Code
+brew install --cask github
+brew install --cask visual-studio-code
