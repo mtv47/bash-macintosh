44e99776da13fa119482a9203b9fd2b24f609176
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-16 18:05:45 +0200

    added some defaults

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+#Set the mac name
 sudo scutil --set ComputerName "macintosh"
 sudo scutil --set LocalHostName "macintosh"
 sudo scutil --set HostName "macintosh"
+
+#Show Path in Finder
+- defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+
+#Sort folders first in Finder
+- defaults write com.apple.finder _FXSortFoldersFirst -bool true; killall Finder
+
+#Enable Key Repeat
+- defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+#Basic.sh
 sudo bash basic.sh
