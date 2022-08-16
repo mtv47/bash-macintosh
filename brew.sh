a6ad44c225dd3eda3d78f526827faf3d8e7f9cd7
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-16 22:33:06 +0200

    added youtube-dl and refactored

--- brew.sh
+++ brew.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#Basic cask
+#Browser
 brew install --cask brave-browser
+
+#Terminal
 brew install --cask kitty
+
+#System Management
 brew install --cask coconutbattery
 brew install --cask turbo-boost-switcher
 brew install --cask amethyst
+
+#Other Apps
 brew install --cask protonmail-bridge
 brew install --cask appcleaner
 brew install --cask balenaetcher
@@ -13,6 +19,10 @@ brew install --cask tailscale
 brew install --cask imageoptim
 brew install --cask dropzone
 brew install --cask vlc
+
+#Youtube-DL
+brew install youtube-dl
+brew install youtube-dl ffmpeg
 
 #Basic installs
 brew install htop
