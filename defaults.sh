a22f22b3313340ab6c1605402e01c6c854b15ff9
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-18 14:41:25 +0200

    added echos

--- defaults.sh
+++ defaults.sh
@@ -1,64 +1,85 @@
 #!/bin/bash
 
-#Show Path in Finder
+echo "Show Path in Finder"
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true; killall Finder
+echo ""
 
-#Sort folders first in Finder
+echo "Sort folders first in Finder"
 defaults write com.apple.finder _FXSortFoldersFirst -bool true; killall Finder
+echo ""
 
-#Enable Key Repeat
+echo "Enable Key Repeat"
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+echo ""
 
-#Disable Disk Warning
+echo "Disable Disk Warning"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.DiskArbitration.diskarbitrationd.plist DADisableEjectNotification -bool YES && sudo pkill diskarbitrationd
+echo ""
 
-#Change Screenshot Default to JPG
+echo "Change Screenshot Default to JPG"
 defaults write com.apple.screencapture type jpg
+echo ""
 
-#Disable the sound effects on boot
+echo "Disable the sound effects on boot"
 sudo nvram SystemAudioVolume=" "
+echo ""
 
-#Shorten autohide animation
+echo "Shorten autohide animation"
 defaults write com.apple.dock autohide-time-modifier -float 0.5 && killall Dock
+echo ""
 
-#Disable show recent
+echo "Disable show recent"
 defaults write com.apple.dock show-recents -bool false && killall Dock
+echo ""
 
-#Minimize animation effect to scale
+echo "Minimize animation effect to scale"
 defaults write com.apple.dock mineffect -string "scale" && killall Dock
+echo ""
 
-#Set the location of screenshots to ~/Pictures
+echo "Set the location of screenshots to ~/Pictures"
 defaults write com.apple.screencapture location -string "$HOME/Pictures" && killall SystemUIServer
+echo ""
 
-#Disable date in screenshots
+echo "Disable date in screenshots"
 defaults write com.apple.screencapture "include-date" -bool "false" 
+echo ""
 
-#Show extensions of files in Finder
+echo "Show extensions of files in Finder"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+echo ""
 
-#Show hidden files in Finder
+echo "Show hidden files in Finder"
 defaults write com.apple.finder AppleShowAllFiles -bool true
+echo ""
 
-#Set the defaults view of Finder to list
+echo "Set the defaults view of Finder to list"
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv" && killall Finder
+echo ""
 
-#Disable the warning when changing a file extension
+echo "Disable the warning when changing a file extension"
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false && killall Finder
+echo ""
 
-#Disable the warning before emptying the Trash
+echo "Disable the warning before emptying the Trash"
 defaults write com.apple.finder WarnOnEmptyTrash -bool false && killall Finder
+echo ""
 
-#Keep folders on top in Desktop
+echo "Keep folders on top in Desktop"
 defaults write com.apple.finder _FXSortFoldersFirst -bool true && killall Finder
+echo ""
 
-#Hide the icons for hard drives, servers, and removable media on the desktop
+echo "Hide the icons for hard drives, servers, and removable media on the desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false && killall Finder
+echo ""
 
-#Don't offer new disks for Time Machine to backup
+echo "Don't offer new disks for Time Machine to backup"
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true && killall SystemUIServer
+echo ""
 
-#Mac App Store update check frequency to 2
+echo "Mac App Store update check frequency to 2"
 defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 2 && killall SystemUIServer
+echo ""
 
-#Expand print dialog by default 
+echo "Expand print dialog by default"
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true && killall SystemUIServer
+echo ""
