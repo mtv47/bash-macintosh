1012a090c25767bf0e791313c4322732a3ec488b
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-08-28 17:53:58 +0200

    added spotify

--- brew.sh
+++ brew.sh
@@ -44,7 +44,7 @@ then
     casks=("brave-browser" "kitty" "coconutbattery" "amethyst" "turbo-boost-switcher" 
     "vlc" "appcleaner" "balenaetcher" "imageoptim" "dropzone" "tailscale"
     "protonvpn" "protonmail-bridge" "youtube-dl" "deluge" "github" 
-    "visual-studio-code")
+    "visual-studio-code" "spotify")
 
     for cask in "${casks[@]}"
     do
