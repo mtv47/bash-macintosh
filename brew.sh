31efa3f6dd685f8e062a28d408ca0cfcab7b37ee
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-09-08 14:11:59 +0200

    added obsidian

--- brew.sh
+++ brew.sh
@@ -44,7 +44,7 @@ then
     casks=("brave-browser" "kitty" "coconutbattery" "amethyst" "turbo-boost-switcher" 
     "vlc" "appcleaner" "balenaetcher" "imageoptim" "dropzone" "tailscale"
     "protonvpn" "protonmail-bridge" "youtube-dl" "deluge" "github" 
-    "visual-studio-code" "spotify" "docker" "librewolf")
+    "visual-studio-code" "spotify" "docker" "librewolf" "obsidian")
 
     for cask in "${casks[@]}"
     do
