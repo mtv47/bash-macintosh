766a36a94d8683332848116a2e27a391f7e05e67
M4Tv3Y <m.tymchenko@protonmail.ch>
2022-09-22 13:51:14 +0200

    added pytorch

--- conda.sh
+++ conda.sh
@@ -4,4 +4,5 @@ echo ""
 echo "Installing Miniconda packages for ml"
 echo ""
 
-conda install --yes numpy jupyter matplotlib Pillow scikit-learn torch
+conda install --yes numpy jupyter matplotlib Pillow scikit-learn
+conda install --yes pytorch torchvision -c pytorch
